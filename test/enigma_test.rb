8bbbc259ffd67efe6295a45b259fafb8308ab05c
Blake <blake.enyart@gmail.com>
2019-02-22 16:39:22 -0700

    Create decrypt test with hardfixed values

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -17,4 +17,9 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
   end
 
+  def test_decrypt_returns_correctly
+    expected = { decryption: "hello world", key: "02715", date: "040895" }
+
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
 end
