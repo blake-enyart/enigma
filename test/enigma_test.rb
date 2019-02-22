f1c7bb2d7fa1e4255e6bd4d115dd1095efe8f257
Blake <blake.enyart@gmail.com>
2019-02-22 16:33:12 -0700

    Create test for existence

---
+++ test/enigma_test.rb
@@ -0,0 +1,23 @@
+require './test/test_helper'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exist
+
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_encrypt_returns_correctly
+    expected = { encryption: "keder ohulw", key: "02715", date: "040895" }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_decrypt_returns_correctly
+    
+  end
+end
