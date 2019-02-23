9b59148f02fe39bd9d71f46c69c66c4c6880b0b6
Blake <blake.enyart@gmail.com>
2019-02-22 18:24:13 -0700

    Create existence test

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -1,4 +1,14 @@
 require './test/test_helper'
 
 class KeyGeneratorTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+    @key_gen = KeyGenerator.new
+  end
+
+  def test_it_exist
+    
+    assert_instance_of KeyGenerator, @key_gen
+  end
 end
