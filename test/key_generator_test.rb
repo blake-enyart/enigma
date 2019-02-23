55c128744c9c45da9db76cae2d1c926313e9c62d
Blake <blake.enyart@gmail.com>
2019-02-22 18:33:59 -0700

    Create test that sampler returns 5 digit number

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -8,7 +8,12 @@ class KeyGeneratorTest < Minitest::Test
   end
 
   def test_it_exist
-    
+
     assert_instance_of KeyGenerator, @key_gen
+  end
+
+  def test_sampler_returns_5_digit_key_generator
+
+    assert_equal 5, @key_gen.key.digits.count
   end
 end
