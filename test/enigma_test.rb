278da75b7801ba3ef1ce84ee32ef0004e8d6bd82
Blake <blake.enyart@gmail.com>
2019-02-23 11:59:57 -0700

    Create attr test for Engima class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -11,6 +11,25 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_attr_are_nil
+
+    assert_equal nil, @enigma.message
+    assert_equal nil, @enigma.key_master
+    assert_equal nil, @enigma.offset_master
+  end
+
+  def test_attr_change_with_encrypt_method
+    assert_equal nil, @enigma.message
+    assert_equal nil, @enigma.key_master
+    assert_equal nil, @enigma.offset_master
+
+    @enigma.encrypt("hello world", "02715", "040895")
+
+    assert_equal "hello world", @enigma.message
+    assert_equal "02715", @enigma.key_master
+    assert_equal "040895", @enigma.offset_master
+  end
+
   def test_encrypt_returns_correctly
     skip
     expected = { encryption: "keder ohulw", key: "02715", date: "040895" }
