90af7fa850807baf2dd48a4caf674761f2d08233
Blake <blake.enyart@gmail.com>
2019-02-23 09:23:26 -0700

    Created instance variables in Enigma class to pass information to CipherMaker class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,16 @@
 class Enigma
+
+  attr_reader :offset_master, :key_master, :offset_master
+
+  def initialize()
+    @message = nil
+    @key_master = nil
+    @offset_master = nil
+  end
+
+  def encrypt(message, key_master, offset_master)
+    @message = message
+    @key_master = key_master
+    @offset_master = offset_master
+  end
 end
