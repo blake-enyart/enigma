f6cb3ede48653df187e7bd935b322cf4adf4703f
Blake <blake.enyart@gmail.com>
2019-02-23 14:08:40 -0700

    Create offset_assignment test

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -20,6 +20,21 @@ class ShiftTest < Minitest::Test
     assert_equal expected, shift.offset_master
   end
 
+  def test_offset_master_converter_generates_correctly
+
+    assert_equal '1025', @shift.offset_master_converter('040895')
+  end
+
+  def test_offset_assigns_each_offset_correctly
+    enigma = Enigma.new(offset_master: '040895')
+    @shift.assign_offset_keys(@shift.offset_master)
+
+    assert_equal '1', offset_generator.a_offset
+    assert_equal '0', offset_generator.b_offset
+    assert_equal '2', offset_generator.c_offset
+    assert_equal '5', offset_generator.d_offset
+  end
+
   def test_attr_return_correctly
 
     assert_equal 3, @shift.a_shift
