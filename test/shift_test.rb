b232ac9e47b9f1169a9047518aa5d1fb4d4cf5b4
Blake <blake.enyart@gmail.com>
2019-02-23 12:15:46 -0700

    Change to attr test to reflect inheritance

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -4,8 +4,8 @@ class ShiftTest < Minitest::Test
 
   def setup
     @enigma = Enigma.new
-    @enigma = Enigma.encrypt("hello world", "02715", "040895")
-    
+    @enigma.encrypt("hello world", "02715", "040895")
+
     @shift = Shift.new
   end
 
@@ -16,8 +16,6 @@ class ShiftTest < Minitest::Test
 
   def test_attr_return_correctly
 
-    assert_equal "02715", @shift.key_master
-    assert_equal "040895", @shift.offset_master
     assert_equal 3, @shift.a_shift
     assert_equal 27, @shift.b_shift
     assert_equal 73, @shift.c_shift
