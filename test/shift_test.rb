742336b37b2188978c94414802b7ac40f46cf174
Blake <blake.enyart@gmail.com>
2019-02-23 11:54:21 -0700

    Create existence test and attribute test for Shift class

---
+++ test/shift_test.rb
@@ -0,0 +1,26 @@
+require './test/test_helper'
+
+class ShiftTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+    @enigma = Enigma.encrypt("hello world", "02715", "040895")
+    
+    @shift = Shift.new
+  end
+
+  def test_it_exist
+
+    assert_instance_of Shift, @shift
+  end
+
+  def test_attr_return_correctly
+
+    assert_equal "02715", @shift.key_master
+    assert_equal "040895", @shift.offset_master
+    assert_equal 3, @shift.a_shift
+    assert_equal 27, @shift.b_shift
+    assert_equal 73, @shift.c_shift
+    assert_equal 20, @shift.d_shift
+  end
+end
