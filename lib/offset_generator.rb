86b36788518c84c7f149efe1ae6a18181d941d4a
Blake <blake.enyart@gmail.com>
2019-02-23 12:43:14 -0700

    Passed offset_master_converter test

--- lib/offset_generator.rb
+++ lib/offset_generator.rb
@@ -1,20 +1,7 @@
-class OffsetGenerator
+module OffsetGenerator
 
-  attr_reader :date, :a_offset, :b_offset, :c_offset, :d_offset
-
-  def initialize(date= Date.today.strftime('%d%m%y'))
-    @date = date
-
-    offset_master = offset_master()
-    @a_offset = offset_master[0]
-    @b_offset = offset_master[1]
-    @c_offset = offset_master[2]
-    @d_offset = offset_master[3]
-
-  end
-
-  def offset_master(date=@date)
-    offset_master = date.to_i**2
+  def offset_master_converter(offset_master=@offset_master)
+    offset_master = offset_master.to_i**2
     offset_master.to_s[-4..-1]
   end
 end
--- test/offset_generator_test.rb
+++ test/offset_generator_test.rb
@@ -3,39 +3,23 @@ require './test/test_helper'
 class OffsetGeneratorTest < Minitest::Test
 
   def setup
-    @enigma = Enigma.new
-    
-    @offset_generator = OffsetGenerator.new
+    @shift = Shift.new
   end
+  
+  def test_offset_master_converter_generates_correctly
 
-  def test_it_exist
-
-    assert_instance_of OffsetGenerator, @offset_generator
-  end
-
-  def test_date_stores_correctly
-
-    assert_equal '040895', OffsetGenerator.new('040895').date
-  end
-
-  def test_defaults_to_today_stored_correctly
-
-    assert_equal 6, @offset_generator.date.length
-    assert_instance_of String, @offset_generator.date
-  end
-
-  def test_offset_master_generates_correctly
-
-    assert_equal '1025', @offset_generator.offset_master('040895')
+    assert_equal '1025', @shift.offset_master_converter('040895')
   end
 
   def test_offset_generator_defaults_to_today
+    skip
     expected = Date.today.strftime('%d%m%y')
 
     assert_equal expected, @offset_generator.date
   end
 
   def test_offset_assigns_each_offset_correctly
+    skip
     offset_generator = OffsetGenerator.new('040895')
 
     assert_equal '1', offset_generator.a_offset
