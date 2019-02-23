9b57af7d0d236e33944eebd37c31fc6454f6a182
Blake <blake.enyart@gmail.com>
2019-02-23 14:19:13 -0700

    Passed assign_offset test

--- lib/shift.rb
+++ lib/shift.rb
@@ -4,13 +4,18 @@ class Shift < Enigma
 
   include OffsetGenerator
 
-  attr_reader :a_shift, :b_shift, :c_shift, :d_shift, :offset_master, :key_master
+  attr_reader :a_shift, :b_shift, :c_shift, :d_shift, :offset_master, :key_master,
+              :a_offset, :b_offset, :c_offset, :d_offset
 
   def initialize(key_master: sampler(), offset_master: Date.today.strftime('%d%m%y'))
     @a_shift = nil
     @b_shift = nil
     @c_shift = nil
     @d_shift = nil
+    @a_offset = nil
+    @b_offset = nil
+    @c_offset = nil
+    @d_offset = nil
     @key_master = key_master
     @offset_master = offset_master
   end
@@ -21,7 +26,9 @@ class Shift < Enigma
   end
 
   def assign_offset_keys(offset_master=@offset_master)
-
-
+    @a_offset = offset_master[0]
+    @b_offset = offset_master[1]
+    @c_offset = offset_master[2]
+    @d_offset = offset_master[3]
   end
 end
