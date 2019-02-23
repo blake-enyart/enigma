1bb06ca46d1c4e54815ea74df89a2bac2d052ea5
Blake <blake.enyart@gmail.com>
2019-02-22 19:09:00 -0700

    Create key_maker method test

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -12,8 +12,14 @@ class KeyGeneratorTest < Minitest::Test
     assert_instance_of KeyGenerator, @key_gen
   end
 
-  def test_sampler_returns_4_element_key_generator
+  def test_sampler_returns_5_element_key_generator
 
-    assert_equal 4, @key_gen.key.count
+    assert_equal 5, @key_gen.sampler
+  end
+
+  def test_key_maker_converts_key_choice_to_keys
+    expected = ['01', '12', '23', '34']
+
+    assert_equal expected, @key_gen.key_maker('01234')
   end
 end
