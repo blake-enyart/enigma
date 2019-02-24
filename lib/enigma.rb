c20bfb327313cb6b6ca629247cd44e1f1ea00b58
Blake <blake.enyart@gmail.com>
2019-02-23 17:52:09 -0700

    Removed OffsetGenerator and KeyGenerator modules and CipherMaker class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,19 +2,14 @@ require './lib/class_helper'
 
 class Enigma
 
-  attr_reader :message, :key_master, :offset_master
+  attr_reader :message, :key_master, :offset_master, :cipher, :encryption
 
   def initialize(offset_master: Date.today.strftime('%d%m%y'), key_master: sampler())
     @key_master = key_master
     @offset_master = offset_master
     @message = nil
-    @cipher = Shift.new(key_master: key_master, offset_master: offset_master)
-  end
-
-  def encrypt(message, key_master=@key_master, offset_master=@offset_master)
-    @message=message
-    @key_master=key_master
-    @offset_master=offset_master
+    @encryption = nil
+    @cipher = Shift.new(key_master: key_master, offset_master: offset_master).shift_master
   end
 
   def sampler
@@ -23,6 +18,24 @@ class Enigma
     key_master = "%05d" % key_master
   end
 
+  def encrypt(message, key_master=@key_master, offset_master=@offset_master)
+    @cipher = Shift.new(key_master: key_master, offset_master: offset_master).shift_master
+    @message=message
+    @key_master=key_master
+    @offset_master=offset_master
+
+    cipher_shift(message, @cipher)
+  end
+
   #Encrypt module
-  
+  def cipher_shift(message, cipher)
+    rotator = [*'a'..'z'] << ' '
+    message.chars.each_with_index do |letter, index|
+      if index
+      letter_location = rotator.index(letter)
+      shifted_rotator = rotator.rotate(cipher['A'])
+      shifted_rotator[letter_location]
+      binding.pry
+    end
+  end
 end
