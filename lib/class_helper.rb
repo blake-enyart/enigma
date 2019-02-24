9a8ca8682162ad5292af621809e5d53921107796
Blake <blake.enyart@gmail.com>
2019-02-23 18:23:31 -0700

    Adjust class_helper

--- lib/class_helper.rb
+++ lib/class_helper.rb
@@ -1,8 +1,5 @@
 require 'pry'
 require 'date'
-#modules
-require './lib/offset_generator'
-require './lib/key_generator'
 #classes in inheritance order
 require './lib/enigma'
 require './lib/shift'
