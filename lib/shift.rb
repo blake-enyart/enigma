0ab3b5b7986257002574be254ff49fa09f3229d1
Blake <blake.enyart@gmail.com>
2019-02-23 17:11:07 -0700

    Rebuilt shift class again becuase class inheritance still wrong...

--- lib/shift.rb
+++ lib/shift.rb
@@ -2,44 +2,28 @@ require './lib/class_helper'
 
 class Shift < Enigma
 
-  include OffsetGenerator
+  attr_reader :shift_master
 
-  attr_reader :a_shift, :b_shift, :c_shift, :d_shift, :offset_master, :key_master,
-              :a_offset, :b_offset, :c_offset, :d_offset, :a_key, :b_key, :c_key,
-              :d_key
-
-  def initialize(key_master: sampler(), offset_master: Date.today.strftime('%d%m%y'))
-    @a_shift = nil
-    @b_shift = nil
-    @c_shift = nil
-    @d_shift = nil
-    @a_offset = nil
-    @b_offset = nil
-    @c_offset = nil
-    @d_offset = nil
-    @a_key = nil
-    @b_key = nil
-    @c_key = nil
-    @d_key = nil
-    assign_key_letters(key_master)
-    assign_offset_letters(offset_master)
-    assign_shift_letters
+  def initialize(key_master:, offset_master:)
+    @shift_master = Hash.new
+    assign_shift_letters(key_master, offset_master)
   end
 
-  def offset_master_converter(offset_master=@offset_master)
-    offset_master = offset_master.to_i**2
-    offset_master.to_s[-4..-1]
+  def assign_shift_letters(key_master, offset_master)
+    offset_values = offset_master_converter(offset_master)
+    key_values = key_master_converter(key_master)
+    shift_keys = [*'A'..'D']
+    shift_keys.each_with_index do |key,index|
+      @shift_master[key] = key_values[index].to_i + offset_values[index].to_i
+    end
   end
 
-  def assign_offset_letters(offset_master)
-    offset_master = offset_master_converter(offset_master)
-    @a_offset = offset_master[0]
-    @b_offset = offset_master[1]
-    @c_offset = offset_master[2]
-    @d_offset = offset_master[3]
+  def offset_master_converter(offset_master)
+    squared_master = offset_master.to_i**2
+    squared_master.to_s[-4..-1]
   end
 
-  def key_maker(key_master)
+  def key_master_converter(key_master)
     choice_array = []
     key_master.chars.each_with_index do |number, index|
       if key_master[index+1] != nil
@@ -47,20 +31,5 @@ class Shift < Enigma
       end
     end
     choice_array
-  end
-
-  def assign_key_letters(key_master)
-    key_array = key_maker(key_master)
-    @a_key = key_array[0]
-    @b_key = key_array[1]
-    @c_key = key_array[2]
-    @d_key = key_array[3]
-  end
-
-  def assign_shift_letters
-    @a_shift = @a_key.to_i + @a_offset.to_i
-    @b_shift = @b_key.to_i + @b_offset.to_i
-    @c_shift = @c_key.to_i + @c_offset.to_i
-    @d_shift = @d_key.to_i + @d_offset.to_i
   end
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -13,55 +13,20 @@ class ShiftTest < Minitest::Test
     assert_instance_of Shift, @shift
   end
 
-  def test_shift_defaults_to_today_for_offset
-    shift = Shift.new
-    expected = Date.today.strftime('%d%m%y')
-
-    assert_equal expected, shift.offset_master
-  end
-
   def test_offset_master_converter_generates_correctly
 
-    assert_equal '1025', @shift.offset_master_converter('040895')
-  end
-
-  def test_offset_assigns_each_offset_correctly
-    @shift.assign_offset_letters('1025')
-
-    assert_equal '1', @shift.a_offset
-    assert_equal '0', @shift.b_offset
-    assert_equal '2', @shift.c_offset
-    assert_equal '5', @shift.d_offset
+    assert_equal '1025', @shift.offset_master_converter(@enigma.offset_master)
   end
 
   def test_key_maker_converts_key_choice_to_keys
     expected = ['02', '27', '71', '15']
 
-    assert_equal expected, @shift.key_maker('02715')
+    assert_equal expected, @shift.key_master_converter(@enigma.key_master)
   end
 
-  def test_assign_key_letters_functions_correctly
-    @shift.assign_key_letters('02715')
+  def test_shift_instantiation_creates_shift_master_hash
+    expected = {"A"=>3, "B"=>27, "C"=>73, "D"=>20}
 
-    assert_equal '02', @shift.a_key
-    assert_equal '27', @shift.b_key
-    assert_equal '71', @shift.c_key
-    assert_equal '15', @shift.d_key
-  end
-
-  def test_shift_defaults_to_5_digit_random_number_for_key_master
-
-    assert_equal 5, Shift.new.key_master.length
-  end
-
-  def test_shift_letters_return_correctly
-    @shift.assign_offset_letters('1025')
-    @shift.assign_key_letters('02715')
-    @shift.assign_shift_letters
-
-    assert_equal 3, @shift.a_shift
-    assert_equal 27, @shift.b_shift
-    assert_equal 73, @shift.c_shift
-    assert_equal 20, @shift.d_shift
+    assert_equal expected, @shift.shift_master
   end
 end
