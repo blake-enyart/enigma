ba7f4686bfbb976b14764452e137c5b6f5c10030
Blake <blake.enyart@gmail.com>
2019-02-24 09:35:02 -0700

    Create test for command_decrypt method

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -108,4 +108,18 @@ class EnigmaTest < Minitest::Test
 
     assert_equal expected, File.readlines('./data/encrypted.txt')[0]
   end
+
+  def test_command_decrypt_decrypts_message_correctly
+    input_1 = 'encrypted.txt'
+    input_2 = 'decrypted.txt'
+    key = "02715"
+    date = "040895"
+    input_array = [input_1, input_2, key, date]
+    enigma = Enigma.new
+
+    enigma.command_decrypt(input_array)
+    expected = "hello world,\n"
+
+    assert_equal expected, File.readlines('./data/decrypted.txt')[0]
+  end
 end
