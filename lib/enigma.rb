e19ddd33b0e99385b407378fc48c671d097e7683
Blake <blake.enyart@gmail.com>
2019-02-24 14:19:58 -0700

    Minor adjust of default offset arguments

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -14,15 +14,16 @@ class Enigma
 
   def initialize
     @rotator = [*'a'..'z'] << ' '
+    @offset_default = Date.today.strftime('%d%m%y')
   end
 
-  def encrypt(message, key_master=sampler(), offset_master=Date.today.strftime('%d%m%y'))
+  def encrypt(message, key_master=sampler(), offset_master=@offset_default)
     cipher = Shift.new(key_master: key_master, offset_master: offset_master).shift_master
     encryption = cipher_shift(message, cipher)
     { encryption: encryption, key: key_master, date: offset_master }
   end
 
-  def decrypt(encryption, key_master=sampler(), offset_master=Date.today.strftime('%d%m%y'))
+  def decrypt(encryption, key_master=sampler(), offset_master=@offset_default)
     cipher = Shift.new(key_master: key_master, offset_master: offset_master).shift_master
     decrypt_cipher = cipher.map { |shift_key| -shift_key }
     decryption = cipher_shift(encryption, decrypt_cipher)
