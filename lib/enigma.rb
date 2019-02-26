3d639eb3ea23b3bb1e65fa504d67de318c4a6608
Blake <blake.enyart@gmail.com>
2019-02-26 15:19:22 -0700

    First attempt at crack method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -29,4 +29,27 @@ class Enigma
     decryption = cipher_shift(encryption, decrypt_cipher)
     { decryption: decryption, key: key_master, date: offset_master }
   end
+
+  def crack(encryption, offset_master=@offset_default)
+    encryption_master = encryption[-4..-1]
+    encryption_key = encryption_master.dup
+
+    key_master = sampler()
+    key_array = [*00001..99999]
+    key_array = key_array.map { |number| "%05d" % number }
+
+    while key_array.length != 0
+      key_master = crack_sampler(key_array)
+      decryption = decrypt(encryption, key_master, offset_master)
+      encryption_key = decryption[:decryption][-4..-1]
+      if encryption_key == ' end'
+        break
+      end
+      key_array.delete(key_master)
+    end
+  end
+
+  def crack_sampler(key_array)
+    key_array.sample
+  end
 end
