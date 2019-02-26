f15f2c1f59b0db9ece348f399cf1248e05e9f0c6
Blake <blake.enyart@gmail.com>
2019-02-26 14:34:03 -0700

    Edit runner files and filepath manipulation for data output

--- lib/file_interface.rb
+++ lib/file_interface.rb
@@ -1,14 +1,12 @@
 module FileInterface
   def read_in_file(file_name)
-    file_path = './data/' + file_name
-    file = File.readlines(file_path)
+    file = File.readlines(file_name)
     file.map { |line| line.chomp }
   end
 
   def create_file(file_name)
-    file_location = './data/' + file_name
-    file = File.open(file_location, 'w')
+    file = File.open(file_name, 'w')
     file.close
-    file_location
+    file_name
   end
 end
